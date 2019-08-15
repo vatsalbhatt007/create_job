5694fe5255e642098797777bce34d67971d49d62
Vatsal Bhatt <vatsal.bhatt13@gmail.com>
2019-08-16 00:28:19 +0200

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-	region = "us-east-1"
-        access_key = "AKIA3MWHQAFB5EJG7Z56"
-	secret_key = "BLJ+vFio5qOBEScQ2pn6zaBM42G+/8VEvTploySr"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-}
